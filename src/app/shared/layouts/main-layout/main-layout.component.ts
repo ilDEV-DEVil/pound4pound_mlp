6f610912c6233fa79d9fa5d4e2938812af8064c4
Nimesh Palakuttige Fernando <84867482+flashbLack98@users.noreply.github.com>
2026-02-10 22:24:35 +0100

    fix: landing on top of page

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
